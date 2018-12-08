29bebbb72d287fe2d8eaa245ff53e780182896c1
Danie B <danie.britto@excelenciaconsulting.com>
2018-12-08 14:23:42 +0530

    ver1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="YouTubeTranscript",
-    version="0.9",
+    version="1.0",
     author="Danie Britto",
     author_email="danieb1996@live.com",
     description="YouTube Transcript Downloader",
